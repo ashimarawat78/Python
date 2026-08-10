341ceb50f0e48530ceb6b0785725d69ee6e28218
Ashima Rawat <ashimarawat78@gmail.com>
2026-08-10 13:53:44 +0530

    Marks_Grades

---
+++ conditional statements/Marks_Grade.py
@@ -0,0 +1,9 @@
+marks = int(input(" enter the marks"))
+if marks>=90:
+         print("A")
+elif marks>=80:
+              print("B")
+elif marks>=70:
+          print("C")
+else :
+    print("FAIL")
