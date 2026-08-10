c1f75a683378e7a0d514f6e2f9570cb32c72a577
Ashima Rawat <ashimarawat78@gmail.com>
2026-08-10 14:14:32 +0530

    Calculator

---
+++ conditional statements/Calculator.py
@@ -0,0 +1,13 @@
+a = int(input("enter first number:"))
+b = int(input("enter second number:"))
+operator= input("enter the operator")
+if operator == "+":
+                print("the ans is :",a+b)
+elif operator == "-":
+                  print("the ans is : ",a-b)
+elif operator == "*":                
+                  print("the ans is :", a*b)
+elif operator == "/":
+                  print("the ans is :", a/b)
+else :
+     print("NO such operator exist ")
