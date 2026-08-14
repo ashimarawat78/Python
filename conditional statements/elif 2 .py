0496ede35d70ec68900bccbcbdf95a0642f5612e
Ashima Rawat <ashimarawat78@gmail.com>
2026-08-14 20:22:28 +0530

    elif 2

--- conditional statements/elif 2 .py
+++ conditional statements/elif 2 .py
@@ -2,7 +2,7 @@ a=int(input("enter the first value :"))
 b=int(input("enter the second value :"))
 c=int(input("enter the third value :"))
 
-if a<+b and a<=c :
+if a<=b and a<=c :
                print("A is the smallest ")
 elif b<=a and b<=c :
                    print("B is the smallest")
