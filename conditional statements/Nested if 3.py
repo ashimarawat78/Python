92c0447eefb7dc84dd47823d313b746163627821
Ashima Rawat <ashimarawat78@gmail.com>
2026-08-15 21:32:39 +0530

    Nested if 3

---
+++ conditional statements/Nested if 3.py
@@ -0,0 +1,9 @@
+a =int(input("enter your age:"))
+if a>=18 :
+         b = int(input("tell your marks:"))
+         if b >=60:
+            print("ELIGILBLE")
+         else :
+            print ("NOT ELIGIBLE")   
+else :
+      priint("NOT ELIGIBLE")
