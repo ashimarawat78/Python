3355b9a61f9e8359d01e643611c0d751a1f4314b
Ashima Rawat <ashimarawat78@gmail.com>
2026-08-15 21:23:59 +0530

    Nested if 1

---
+++ conditional statements/Nested if 1.py
@@ -0,0 +1,11 @@
+a =int(input("enter the value:"))
+if a>0 :
+    print("the number is positive")
+    if a % 2==0 :
+                print("the number is even")
+    else :      
+                print("the number is odd")   
+elif a<0:
+         print("the number is negative")
+else :
+    print("the nummber is zero")
