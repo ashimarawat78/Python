8e6e9d4f13f6983b86087c894c358219f382ee80
Ashima Rawat <ashimarawat78@gmail.com>
2026-08-15 21:38:07 +0530

    Nested if 4

---
+++ conditional statements/Nested if 4.py
@@ -0,0 +1,9 @@
+a =input("enter your username:")
+if a=="admin" :
+         b = int(input("your password please:"))
+         if b == 1234:
+            print("Login Successful")
+         else :
+             print("Login NOT Successful")   
+else :
+      print("Login NOT Successful")
