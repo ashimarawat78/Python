b396f2230e5f810d1af7102ad19d0a07eeacedaf
Ashima Rawat <ashimarawat78@gmail.com>
2026-08-15 21:27:11 +0530

    Nested if 2

---
+++ conditional statements/Nested if 2.py
@@ -0,0 +1,11 @@
+a =int(input("enter the value:"))
+if a>0 :
+    print("the number is positive")
+    if a > 100 :
+                print("the number is positive and GREATER than 100")
+    else :      
+                print("the number is positive but not GREATER than 100")   
+elif a<0:
+         print("the number is negative")
+else :
+    print("the nummber is zero")
