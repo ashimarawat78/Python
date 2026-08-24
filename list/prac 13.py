b9baaa97c0b84a6dd8fb76cc5e13af44492e48bc
Ashima Rawat <ashimarawat78@gmail.com>
2026-08-24 18:13:03 +0530

    prac 13

---
+++ list/prac 13.py
@@ -0,0 +1,5 @@
+numbers = [4, 7, 12, 9, 20, 15, 2]
+for i in range(len(numbers)):
+                             if numbers[i] % 2==0:
+                                               numbers[i]=numbers[i]*numbers[i]
+print(numbers)
