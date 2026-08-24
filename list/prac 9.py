da99849df7393d8fbc4b0b346fde55c32fd06d11
Ashima Rawat <ashimarawat78@gmail.com>
2026-08-24 13:51:30 +0530

    prac 9

---
+++ list/prac 9.py
@@ -0,0 +1,8 @@
+numbers = [4, 7, 12, 4, 20, 7, 15, 12]
+a=int(input("enter the value:"))
+count=0
+for i in numbers:
+                 if i==a:
+                      count=count+1
+               
+print(count)
