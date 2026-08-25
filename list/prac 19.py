a75e949473dfe3794c013eaa42d8dccf53ae42ba
Ashima Rawat <ashimarawat78@gmail.com>
2026-08-25 18:08:55 +0530

    prac 19

---
+++ list/prac 19.py
@@ -0,0 +1,6 @@
+numbers = [10, 25, 7, 30, 25, 18, 25]
+a=int(input("enter the value:")) 
+for i in range(len(numbers)):
+                  if numbers[i]==a:
+                    print(i)
+                    break
